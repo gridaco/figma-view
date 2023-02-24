4f66358d10f0fb2bc4cdb73a6b5c7eac3dcd5b2d
ywnam123 <ywnam@hatiolab.com>
2023-02-24 09:25:56 +0900

    fix rect background color

--- packages/figma-view-react/lib/nodes/rectangle.tsx
+++ packages/figma-view-react/lib/nodes/rectangle.tsx
@@ -7,15 +7,14 @@ export function FigmaRectangleNode({
 }: NodeViewProps<RectangleNode>) {
   const { x, y, width, height, fills } = data;
   const color = fills?.[0]?.color ?? { r: 0, g: 0, b: 0, a: 0 };
-
   return (
     <div
       id={data.id}
       style={{
         position: "absolute",
-        color: `rgba(${color.r * 255}, ${color.g * 255}, ${color.b * 255}, ${
-          color.a
-        })`,
+        backgroundColor: `rgba(${color.r * 255}, ${color.g * 255}, ${
+          color.b * 255
+        }, ${color.a})`,
         top: y,
         left: x,
         width,
